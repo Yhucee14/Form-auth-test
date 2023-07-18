1333a14e39e27434641204f18dc44fc43b0f104f
Yhucee14 <uchechukwu7814@gmail.com>
2023-07-18 12:53:47 -0700

    corrected importation error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Dashboard from './components/Dashboard'
-import Auth from './components/auth';
+import Auth from './components/Auth';
 import './App.css'; 
 
 import { useUserContext } from "./context/userContext"
